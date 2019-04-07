d59e7fd2144adfe3e6d4ef9fbf12d9b34a3e593e
Eden Ramoneda <edenlee143@gmail.com>
2019-04-07 21:10:50 +0800

    fixed public folder

--- controllers/studentregistrationController.js
+++ controllers/studentregistrationController.js
@@ -2,7 +2,9 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res)=> {
-    res.render('registration');
+    res.render('index/registration',{
+      FormTitle : "Student Registration"
+    });
   });
  
 module.exports = router; 
--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 
 const studentRegController = require('./controllers/studentregistrationController');
-
+//const MinhoMainsPage = require('./controllers/')
 var app = express();
 
-app.set('views', path.join(__dirname, '/views/'))
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/'}));//config details for handlebars
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts/' }));//config details for handlebars
 app.set('view engine', 'hbs');
 app.listen(3000, () => {
     console.log("Server started at port : 3000");
 });
 
 app.use('/registration', studentRegController);
+app.use(express.static(path.join(__dirname, 'public')));
